18d812257afbe18172bf869b78a991f8b5965505
MukeshSmart <mukesh14mca60@gmail.com>
2021-10-15 00:24:30 +0530

    dev-branch commit 2

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,6 +14,23 @@ namespace my_book.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book_Author>()
+                 .HasOne(b => b.Book)
+                 .WithMany(a => a.Book_Authors)
+                 .HasForeignKey(f => f.BookId);
+
+            modelBuilder.Entity<Book_Author>()
+                 .HasOne(b => b.Author)
+                 .WithMany(a => a.Book_Authors)
+                 .HasForeignKey(f => f.AuthorId);
+        }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book_Author> Book_Authors { get; set; }
+
+
     }
 }
