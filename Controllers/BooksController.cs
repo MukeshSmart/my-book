9832ed7c20ede4d39d3f2e44599691487310b38a
MukeshSmart <mukesh14mca60@gmail.com>
2021-10-14 23:34:48 +0530

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using my_book.Data.Services;
+using my_book.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_book.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BookSevice _bookSevice;
+        public BooksController(BookSevice bookSevice)
+        {
+            _bookSevice = bookSevice;
+        }
+
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _bookSevice.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookSevice.GetBookById(id);
+            return Ok(book);
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _bookSevice.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id,[FromBody] BookVM book)
+        {
+           var updateBook = _bookSevice.UpdateBookById(id,book);
+            return Ok(updateBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _bookSevice.DeleteBookById(id);
+            return Ok();
+        }
+    }
+}
---
+++ Data/Services/BookSevice.cs
@@ -0,0 +1,71 @@
+﻿using my_book.Data.Models;
+using my_book.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_book.Data.Services
+{
+    public class BookSevice
+    {
+        private AppDbContext _context;
+        public BookSevice(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                Description = book.Description,
+                DateAdded = DateTime.Now
+
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks() => _context.Books.ToList();
+
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(x=>x.Id == bookId);
+
+        public Book UpdateBookById(int bookId,BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(x => x.Id == bookId);
+            if(_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Author = book.Author;
+                _book.CoverUrl = book.CoverUrl;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.Description = book.Description;
+
+                _context.SaveChanges();
+            }
+            return _book;
+        }
+
+        public void DeleteBookById(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(x => x.Id == bookId);
+            if (_book != null)
+            {
+
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
